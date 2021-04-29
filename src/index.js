821175830459acd1aecfa6104209e98b0392a518
Alex Scott <alex.scott@civicactions.com>
2021-04-29 15:23:31 +0200

    P PeerJS setup and working in Web Browser client.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ const port = 4001;
 const http = require("http");
 const server = http.createServer(app);
 
-// Also setup WEB frontend for testing:
+// Also setup a WEB frontend
+// I have added it to the server repo but it's really a frontend client.
 const { v4: uuidV4 } = require('uuid');
 app.get('/', (req, res) => {
   res.redirect(`/${uuidV4()}`);
@@ -12,54 +13,31 @@ app.get('/', (req, res) => {
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
+// This will be the start of having different rooms for different journeys:
 app.get('/:room', (req, res) => {
   res.render('room', { roomId: req.params.room });
 });
 
+// Try to switch off the error. Might be easier to add a favicon!
 app.get('/favicon.ico', (req, res) => res.status(204));
 // End web frontend.
 
-// TODO What does the origins do when there is no port?
+// Connect Socket IO to our app.
 const io = require("socket.io")(server, { origins: "*:*" });
 app.use(express.static(__dirname + "/public"));
 
 io.sockets.on("error", (e) => console.log(e));
 server.listen(port, () => console.log(`Server is running on port ${port}`));
 
-let broadcaster;
+// Handle websocket communications via socket.io
+io.on('connection', socket => {
+  console.log("Connection made");
+  socket.on('join-room', (roomId, userId) => {
+    socket.join(roomId);
+    socket.to(roomId).broadcast.emit('user-connected', userId);
 
- // TODO, why io.sockets not just io?
-io.sockets.on("connection", (socket) => {
-  console.log("A client connected to Socket");
-
-socket.on("frontendConnected", () => {
-  console.log("The server says that the frontend connected");
-});
-
-  socket.on("frontendMobileConnected", () => {
-  console.log("The server says that the frontend mobile connected");
-  });
-
-  socket.on("broadcaster", () => {
-    broadcaster = socket.id;
-    socket.broadcast.emit("broadcaster");
-  });
-  socket.on("watcher", () => {
-    socket.to(broadcaster).emit("watcher", socket.id);
-  });
-  socket.on("disconnect", () => {
-    socket.to(broadcaster).emit("disconnectPeer", socket.id);
-  });
-  socket.on("offer", (id, message) => {
-    socket.to(id).emit("offer", socket.id, message);
-  });
-  socket.on("answer", (id, message) => {
-    socket.to(id).emit("answer", socket.id, message);
-  });
-  socket.on("candidate", (id, message) => {
-    socket.to(id).emit("candidate", socket.id, message);
-  });
-  socket.on('comment', (id, message) => {
-    socket.to(id).emit("comment", socket.id, message);
+    socket.on('disconnect', () => {
+      socket.to(roomId).broadcast.emit('user-disconnected', userId);
+    });
   });
 });
